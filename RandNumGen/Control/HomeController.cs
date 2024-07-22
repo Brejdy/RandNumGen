e314a9af7391acc846fd66bf73c1638fb66237d8
janjb <janjb@HONZALAPTOP>
2024-07-22 12:49:54 +0200

    Add project files.

---
+++ RandNumGen/Control/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RandNumGen.Model;
+
+namespace RandNumGen.Control
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Generator generator = new Generator();
+            ViewBag.Cislo = generator.vratCislo();
+            return View();
+        }
+    }
+}
---
+++ RandNumGen/Model/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace RandNumGen.Model
+{
+    public class Generator
+    {
+        private readonly Random random = new Random();
+        public int vratCislo()
+        {
+            return random.Next(100);
+        }
+    }
+}
